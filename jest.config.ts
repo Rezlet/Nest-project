efea9bcc9bf1e6bbf427693c9c55c3e04317d537
Rezlet <83124881+Rezlet@users.noreply.github.com>
2025-07-08 23:37:58 +0700

    chore: add github ci pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,16 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    collectCoverageFrom: ['**/*.ts'],
-    coverageDirectory: '../coverage',
-    coverageReporters: ['html', 'text-summary'],
-    moduleNameMapper: {
-      '^src/(.*)$': '<rootDir>/$1',
-    },
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.ts'],
+  coverageDirectory: '../coverage',
+  coverageReporters: ['html', 'text-summary'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
